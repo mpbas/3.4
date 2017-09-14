3ac41ff318a22275c6f3d0f91c9cca97e007df7f
Maxim Baskakov <mpbaskakov@gmail.com>
2017-09-15 00:08:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from pprint import pprint
+import time
+from urllib.parse import urlencode
+import requests
+
+AUTHORIZE_URL = 'https://oauth.vk.com/authorize'
+CLIENT_ID = 6183616
+ACCESS_TOKEN = '0da3da20deb6c1f2bb6877bb36cc51239e240e56720caac24c0ea4879cc89f3b07e0173c2f0ef775ade9c' # enter your token here
+
+
+def auth():
+    auth_data = {
+        'client_id': CLIENT_ID,
+        'display': 'popup',
+        'response_type': 'token',
+        'scope': 'status,friends',
+        'v': '5.68'
+    }
+    token_url = '?'.join((AUTHORIZE_URL, urlencode(auth_data)))
+    return token_url
+
+
+def get_friends(user_id):
+    params = {
+        'user_id': user_id,
+        'access_token': ACCESS_TOKEN,
+        'v': '5.68'
+    }
+    response = requests.get('https://api.vk.com/method/friends.get', params)
+    if user_id == 0:
+        friends = dict.fromkeys(response.json()['response']['items'])
+    else:
+        friends = response.json()['response']['items']
+    return friends
+
+
+def get_mutual_friends():
+    friends = get_friends(0)
+    for user in friends.keys():
+        try:
+            friends[user] = get_friends(user)
+            time.sleep(0.35)
+        except KeyError:
+            pass
+    # none_friends = [f for f in friends.keys() if friends[f] == None]
+    # for f in none_friends:
+    #     friends[f] = get_friends(f)
+    #     time.sleep(0.35)  # ничего не помогает, исключения всё равно выскакивают, я не знаю что тут сделать можно.
+    # for f in friends.keys():
+    #     n = 0
+    #     while n <= len(friends.keys()):
+    #         try:
+    #             result = list(set(friends[f]) & set(friends[n]))
+    #             n += 1
+    #             print(result)
+    #         except Exception:
+    #             pass
+    # pprint(friends)
+
+get_mutual_friends()
